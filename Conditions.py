7cab8984a049bb5c02e88fa809289a39d9550f79
husnain <husnain.babar@hubextech.com>
2024-12-23 14:53:44 +0500

    Sets in py

---
+++ Conditions.py
@@ -0,0 +1,44 @@
+name = 'Husnain'
+# if name == 'Husnain':
+#     print('True')
+# else:
+#     print('False')
+#
+#     name = 'Husnain'
+#     if name == 'Husnain':
+#         print('True')
+#     elif name == 'zaman':
+#         print('name is zaman')
+#     else:
+#         print('False')
+
+
+        #Boolean
+        #and or not like
+
+if name == 'Husnain' or name == 'zaman':
+ print('True')
+else:
+        print('False')
+
+        # if name == 'Husnain' and name == 'zaman':
+        #     print('True')
+        # else:print('False')
+        # if name == 'Husnain' not name == 'zaman':
+        #     print('True')
+
+# Is python
+a = [1,2,3]
+b = [1,2,3]
+print(a==b)
+# but when I use is it's gonna false
+
+print(a is b)
+#because is check in memory not check value and in memory we know they exist in different places
+
+#false values
+#false
+#none
+#Zero
+#any empty sequence  ""()[]
+#any empty mapping  {}
