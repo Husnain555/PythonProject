9aebb8e9e5fca916f044400a5acb83eda42873b0
husnain <husnain.babar@hubextech.com>
2024-12-23 18:36:11 +0500

    conditions in py

---
+++ loops.py
@@ -0,0 +1,28 @@
+num = [1,2,3,4,5,6,7,8,9]
+# for i in num:
+#     for letter in 'abcdefghi':
+#         print(letter,i)
+#     #if i == 3:
+#      #  print('fount it')
+#        # continue
+#     # print(i)
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+# for a in alpha:
+#     if a == 'a':
+#         print(a)
+#         continue
+#
+#     print(a)
+
+
+# while a :
+#     if 'a' >= 'a':
+#         print(a)
+#         continue
+#     print(a)
+
+x = 1
+while True:
+    print(x)
+    x+=1
