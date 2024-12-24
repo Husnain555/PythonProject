096c14cfd55efd5fa98eaf3a7ff65b1e64d464d4
husnain <husnain.babar@hubextech.com>
2024-12-24 11:59:58 +0500

    import export in py

--- import_module/import1.py
+++ import_module/import1.py
@@ -1,5 +1,27 @@
 import export1
-
+import random
+import math
+import datetime
+import calendar
+import os
+import antigravity
 text = ['Husnain', 'Babar', 'awais', 'zaman']
 find1 = export1.find_index(text, 'zaman1')
 print(find1)
+
+random_text = random.choice(text)
+print(random_text)
+
+num = math.sin(91)
+print(num)
+
+
+print(datetime.datetime.now())
+
+calendar = calendar.isleap(2020)
+print(calendar)
+
+print(os.getcwd())
+print(os.__file__)
+
+print(antigravity)
