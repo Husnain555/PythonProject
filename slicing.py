313e85ce7f42e6fdf1aa544ca59022a3a7b4de4e
talk2 <talk2husnain@gmail.com>
2024-12-24 14:34:28 +0500

    slicing in py

---
+++ slicing.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9]
+
+
+
+print(list)
+print(list[0])
+print(list[1])
+print(list[-1])
+print(list[1:2])
+print(list[1:-2])
+print(list[-1:2])
+print(list[-1:0])
+print(list[2:-1:2])   #its print every second value
+
+
+
+string = 'www.google.com'
+print(string[-4:])  #its get .com from the string
