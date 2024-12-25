7dc4ba2d7eef479eee106773554138578a357ac4
talk2 <talk2husnain@gmail.com>
2024-12-25 17:18:50 +0500

    Date and time modulation

--- work with dates and times.py
+++ work with dates and times.py
@@ -1,5 +1,9 @@
 import datetime
+import pytz
 date = datetime.datetime.now()
+date_today = datetime.datetime.today()
+date_utc = datetime.datetime.utcnow()
+print('Here the all different time zones',date,date_today,date_utc)
 print(date)
 date_in_string = date.strftime("%d/%m/%Y %H:%M:%S")
 print(date_in_string)
@@ -9,3 +13,17 @@ time_stamp = date.timestamp()
 print(time_stamp)
 time_stamp_in_string = datetime.datetime.fromtimestamp(time_stamp)
 print(time_stamp_in_string)
+print(date.year)
+print(date.day)
+print(date.timestamp())
+print(date.month)
+
+# Use of PYTZ
+
+#
+dt = datetime.datetime.now(pytz.timezone('Asia/Karachi'))
+print(dt)
+
+
+for tz in pytz.all_timezones:
+    print(tz)
