8ea9df4d0ffeef9d8e916ce6efef393ca1e24d6b
talk2 <talk2husnain@gmail.com>
2024-12-26 19:20:57 +0500

    ranom values and automation and rename file through py

--- generate random number.py
+++ generate random number.py
@@ -18,3 +18,15 @@ print(value6)
 deck = list(range(1,52))
 random.shuffle(deck)
 print(deck)
+
+
+# fake detail card make
+
+
+for num in range(100):
+    first_name = random.choice(['Husnain','Babar','Hasan'])
+    last_name = random.choice(['Said','Ali','Hassan'])
+    address = random.choice(['Safi','Khan','Mohammed'])
+    email = first_name.lower()+'.'+last_name.lower()+'@gmail.com'
+    phone_number = '01'+str(random.randint(100000000,999999999))
+    print(f'{first_name} {last_name} \n{address} \n{email} \n{phone_number}')
